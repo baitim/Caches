9b5361582784bddd9bde0d2c459a9f12d0b7cca0
Тимур Байдюсенов <63646703+baitim@users.noreply.github.com>
2024-09-16 10:15:54 +0300

    generate two disjoint sets -> arc better lfu

--- tests/generate.py
+++ tests/generate.py
@@ -7,13 +7,18 @@ for i in range(0, count_tests) :
 
     size_of_cache  = random.randint(1000, 2000)
     count_of_elems = random.randint(10000, 20000)
-    test_str = str(size_of_cache) + " " + str(count_of_elems) + "\n"
+    test_str = str(size_of_cache) + " " + str(count_of_elems * 2) + "\n"
 
     print("count: ", count_of_elems / 10000)
     for j in range(count_of_elems) :
         if (j % 10000 == 0) :
             print("elems: ", j / 10000)
         test_str += str(random.randint(1, 3500)) + " "
+
+    for j in range(count_of_elems) :
+        if (j % 10000 == 0) :
+            print("elems: ", j / 10000)
+        test_str += str(random.randint(3500, 7000)) + " "
 
     file.write(test_str)
     file.close()
